704bc867999fa1ec10e8091b4c9357f1d7a26ec6
Jeremy Carriere <jeremyo.carriere@gmail.com>
2023-02-08 19:12:10 +0100

    setup footer

--- my-portfolio/src/components/Footer/Footer.js
+++ my-portfolio/src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
+import { ReactComponent as GithubLogo } from '../../assets/icons/icons8-github.svg'
+import { ReactComponent as LinkedinLogo } from '../../assets/icons/icons8-linkedin.svg'
 export default function Footer() {
     return (
         <footer className="footer">
-            <h1 className="footer_text">Footer</h1>
-            
+            <h1 className="footer__text">© Jérémy C. - Portfolio</h1>
+            <div>
+                <GithubLogo className="footer__icon"/>
+                <LinkedinLogo className="footer__icon"/>
+            </div>
         </footer>
     )
 }
